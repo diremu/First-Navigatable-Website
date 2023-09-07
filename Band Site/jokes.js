a8b034fd47d4e111f191bdbc2b6babe4c4d1a941
Diremu <diremuadebanjo@gmail.com>
2023-09-07 21:20:29 +0100

    further commit

---
+++ Band Site/jokes.js
@@ -0,0 +1,8 @@
+var removeCartItemButtons = document.getElementsByClassName('btn-danger');
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove();
+    })
+}
